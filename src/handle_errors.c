3fd4d2ca4c025152120b3e4e1f27802a2b9266c3
Bram Provoost <bprovoos@f1r2s16.codam.nl>
2022-10-14 14:02:25 +0200

    cleaner error msg

--- src/handle_errors.c
+++ src/handle_errors.c
@@ -6,39 +6,29 @@
 /*   By: bprovoos <bprovoos@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/28 13:32:47 by bprovoos      #+#    #+#                 */
-/*   Updated: 2022/10/06 11:25:21 by bprovoos      ########   odam.nl         */
+/*   Updated: 2022/10/14 14:01:31 by bprovoos      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-int	msg_error(char *err)
-{
-	int	nr;
-
-	nr = errno;
-	write(2, "pipex: ", 7);
-	write(2, strerror(errno), ft_strlen(strerror(errno)));
-	write(2, ": ", 2);
-	write(2, err, ft_strlen(err));
-	write(2, "\n", 1);
-	return (nr);
-}
-
 int	msg_error_code(char *err, int code)
 {
-	write(2, "pipex: ", 7);
-	write(2, strerror(errno), ft_strlen(strerror(errno)));
-	write(2, ": ", 2);
-	write(2, err, ft_strlen(err));
-	write(2, "\n", 1);
+	ft_putstr_fd("pipex: ", 2);
+	ft_putstr_fd(strerror(errno), 2);
+	ft_putstr_fd(": ", 2);
+	ft_putendl_fd(err, 2);
 	return (code);
+}
+
+int	msg_error(char *err)
+{
+	return (msg_error_code(err, errno));
 }
 
 int	msg_custom_error_code(char *err, char *cmd, int code)
 {
-	write(2, err, ft_strlen(err));
-	write(2, cmd, ft_strlen(cmd));
-	write(2, "\n", 1);
+	ft_putstr_fd(err, 2);
+	ft_putendl_fd(cmd, 2);
 	return (code);
 }
